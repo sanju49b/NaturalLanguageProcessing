3f248046b64237c28453011e1b44bc2906bbbfcc
sanju49b <143645105+sanju49b@users.noreply.github.com>
2025-01-19 20:00:11 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+# https://www.youtube.com/watch?v=hff2tHUzxJM&list=PLc2rvfiptPSSpZ99EnJbH5LjTJ_nOoSWW
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from transformers import pipeline
+
+st.title('Fine Tuning BERT for Twitter Tweets for multi class sentiment classification')
+
+classifier = pipeline('text-classification', model= 'bert-base-uncased-sentiment-model')
+
+text=st.text_area("enter the tweet to classify")
+
+if st.button("Predict"):
+    result=classifier(text)
+    st.write(result)
+    
+
